8d5ff06e56a8cf764b386eff7f789c689ea2810b
Mordecaied <motta.josef@gmail.com>
2018-10-03 22:40:44 -0400

    Get user email

--- app/src/main/java/com/example/owner/androidlab01/StartActivity.java
+++ app/src/main/java/com/example/owner/androidlab01/StartActivity.java
@@ -14,6 +14,7 @@ public class StartActivity extends Activity{
 
     protected static final String ACTIVITY_NAME = "StartActivity";
     private Button buttonXYZ;
+    private Button chatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,18 @@ public class StartActivity extends Activity{
         setContentView(R.layout.activity_start);
         Log.i(ACTIVITY_NAME, "In onCreate()");
 
+        //START CHAT BUTTON
+        chatButton = (Button)findViewById(R.id.chatButton);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(chatButton != null){
+                    Log.i(ACTIVITY_NAME, "User clicked Start Chat");
+                }
+            }
+        });
+
+        //IM A BUTTON
         buttonXYZ = (Button) findViewById(R.id.testButton);
 
         buttonXYZ.setOnClickListener(new View.OnClickListener() {
